05c765adb12a59f45de4977c92352e76a94d4951
satya <a.yvmsatyasai@gmail.com>
2024-09-14 17:13:07 +0530

    .\DataTypes_2.java file modified.

--- DataTypes_2.java
+++ DataTypes_2.java
@@ -1,17 +1,17 @@
 public class DataTypes_2 {
 
 	public static void main(String[] args) {
-		int num1=9;
-		byte by=127;
-		short sh=558;
-		long l=5854l;
+		// int num1=9;
+		// byte by=127;
+		// short sh=558;
+		// long l=5854l;
 		
-		float f=5.8f;
-		double d=5.8;
+		// float f=5.8f;
+		// double d=5.8;
 		
-		char c='k';
+		// char c='k';
 		
-		boolean b=true;
+		// boolean b=true;
 
 		System.out.println("Values are not printed..");
 
